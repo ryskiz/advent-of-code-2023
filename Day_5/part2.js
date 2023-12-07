5e07193486ab35cd442fef434a59eb2b2473cc13
Ryan Skinner <ryan@hypercomply.com>
2023-12-07 02:13:10 -0500

    day 7

--- Day_5/part2.js
+++ Day_5/part2.js
@@ -1,1 +1,114 @@
 // dear god
+const lines = [
+  "seeds: 79 14 55 13",
+  "",
+  "seed-to-soil map:",
+  "50 98 2",
+  "52 50 48",
+  "",
+  "soil-to-fertilizer map:",
+  "0 15 37",
+  "37 52 2",
+  "39 0 15",
+  "",
+  "fertilizer-to-water map:",
+  "49 53 8",
+  "0 11 42",
+  "42 0 7",
+  "57 7 4",
+  "",
+  "water-to-light map:",
+  "88 18 7",
+  "18 25 70",
+  "",
+  "light-to-temperature map:",
+  "45 77 23",
+  "81 45 19",
+  "68 64 13",
+  "",
+  "temperature-to-humidity map:",
+  "0 69 1",
+  "1 0 69",
+  "",
+  "humidity-to-location map:",
+  "60 56 37",
+  "56 93 4",
+];
+
+const part2 = () => {
+  const seedNumbers = lines.shift().split("seeds: ")[1].split(" ");
+  const seedRanges = getSeedRanges(seedNumbers);
+  const categoryRegex = /\-to\-/g;
+  const categoryMappings = {};
+
+  let currentCategory = null;
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+
+    // Reset category on empty line
+    if (line === "") {
+      currentCategory = null;
+      continue;
+    }
+
+    // Set current category on category line
+    if (line.match(categoryRegex)) {
+      currentCategory = line.split(" map:")[0];
+      continue;
+    }
+
+    // Init category
+    categoryMappings[currentCategory] = categoryMappings[currentCategory] || [];
+
+    // push line to that category
+    const [destination, source, range] = lines.split(' ')
+    categoryMappings[currentCategory].push([+destination, +source, +range]);
+  }
+
+  const categoryProgression = Object.keys(categoryMappings);
+  let lowestNum = null;
+  for (let i = 0; i < seedNumbers.length; i++) {
+    const seedRange = seedNumbers[i];
+
+    // if (!lowestNum) lowestNum = locationNumber;
+    // lowestNum = Math.min(lowestNum, locationNumber);
+  }
+
+  return lowestNum;
+};
+
+function getInRangeSeeds(seedRangeNumbers, sourceRangeNumbers) {
+  const [seedStart, seedRange] = seedRangeNumbers
+  const [sourceStart, sourceRange] = sourceRangeNumbers
+  const seedMax = seedStart + seedRange
+  const sourceMax = sourceStart + sourceRange
+  let outliers = []
+  let overlapped = null
+  let overlappedStart = null
+
+  // In between
+  if (seedStart >= sourceStart && seedMax <= sourceMax) {
+    if (seedStart !== sourceStart) outliers.push([sourceStart, (seedStart - 1) - sourceStart])// outlier left
+    if (seedMax !== sourceMax) outliers.push([seedMax + 1, ]) // outlier right
+    const outlierLeft = []
+    const outlierRight = []
+    outliers = [outlierLeft, outlierRight]
+    overlapped = { start: seedStart, range: seedRange }
+
+  }
+
+  return {
+    overLapped,
+    outliers: []
+  }
+}
+
+function getSeedRanges(seeds) {
+  const seedRanges = [];
+  for (let i = 1; i < seeds.length; i += 2) {
+    seedRanges.push([+seeds[i - 1], +seeds[i]]);
+  }
+  return seedRanges;
+}
+
+console.log(part2());
---
+++ Day_7/part2.js
@@ -0,0 +1,111 @@
+const part2 = () => {
+  lines.sort((a, b) => {
+    const handA = a.split(" ")[0];
+    const handB = b.split(" ")[0];
+    return sortTwoHands(handA, handB);
+  });
+
+  lines.reverse();
+  let totalWinnings = 0;
+  for (let i = 0; i < lines.length; i++) {
+    const bid = lines[i].split(" ")[1];
+    totalWinnings += (bid * (i + 1));
+  }
+
+  return totalWinnings;
+};
+
+const sortTwoHands = (handA, handB) => {
+  const permutationsA = getAllPossibleValues(handA)
+  const permutationsB = getAllPossibleValues(handB)
+  const handValueA = permutationsA.map((hand) => determineHandValue(hand)).sort((a, b) => b - a)[0]; // Too lazy to make faster
+  const handValueB = permutationsB.map((hand) => determineHandValue(hand)).sort((a, b) => b - a)[0];
+  if (handValueA > handValueB) return -1; // Hand A wins
+  if (handValueB > handValueA) return 1; // Hand b wins
+  return determineTieBreaker(handA, handB); // -1 hand A wins 1 hand B wins
+};
+
+const containsWildCard = /J/
+const getAllPossibleValues = (hand) => {
+  if (!containsWildCard.test(hand)) {
+    return [hand]
+  }
+
+  const handSet = getHandSet(hand)
+  const permutations = []
+  for (let key in handSet) {
+    const s = hand
+    permutations.push(s.replace(/J/g, key))
+  }
+
+  return permutations
+}
+
+/*
+Five of a kind = 7
+Four of a kind = 6
+Full house = 5
+Three of a kind = 4
+Two pair = 3
+One pair = 2
+High card = 1
+*/
+const determineHandValue = (hand) => {
+  const handSet = getHandSet(hand)
+
+  let hasPair = false;
+  let hasThreeOfAKind = false;
+  for (let key in handSet) {
+    const val = handSet[key];
+    if (val === 5) {
+      return 7; // Five of a kind
+    } else if (val === 4) {
+      return 6; // Four of a kind
+    } else if ((val === 3 && hasPair) || (val === 2 && hasThreeOfAKind)) {
+      return 5; // Full house
+    } else if (val === 2 && hasPair) {
+      return 3; // Two pair
+    } else if (val === 3) {
+      hasThreeOfAKind = true;
+    } else if (val === 2) {
+      hasPair = true;
+    }
+  }
+
+  if (hasThreeOfAKind) return 4;
+  if (hasPair) return 2;
+  return 1; // high card
+};
+
+const getHandSet = (hand) => {
+  const handSet = {};
+  for (let i = 0; i < hand.length; i++) {
+    handSet[hand[i]] = handSet[hand[i]] || 0;
+    handSet[hand[i]] += 1;
+  }
+  return handSet
+}
+
+const faceCardVals = {
+  T: 10,
+  J: 1,
+  Q: 12,
+  K: 13,
+  A: 14,
+};
+
+const determineTieBreaker = (handA, handB) => {
+  for (let i = 0; i < handA.length; i++) {
+    const cardA = handA[i];
+    const cardB = handB[i];
+    const handAVal = isNaN(+cardA) ? faceCardVals[cardA] : +cardA;
+    const handBVal = isNaN(+cardB) ? faceCardVals[cardB] : +cardB;
+
+    if (handAVal > handBVal) return -1;
+    if (handBVal > handAVal) return 1;
+  }
+
+  return 0;
+};
+
+console.log(part2());
